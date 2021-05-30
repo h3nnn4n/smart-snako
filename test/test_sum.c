623e7df12a322d71b93efcd1412c699034775ef5
h3nnn4n <uber.renan@gmail.com>
2021-05-29 23:57:24 -0300

    Add test

---
+++ test/test_sum.c
@@ -0,0 +1,23 @@
+#include <unity.h>
+
+#include <config.h>
+#include <sum.h>
+
+void test_one_plus_two_equals_three() {
+    int x      = 1;
+    int y      = 2;
+    int result = sum(x, y);
+
+    TEST_ASSERT_TRUE(result == 3);
+}
+
+void setUp() { init_config(); }
+void tearDown() {}
+
+int main() {
+    UNITY_BEGIN();
+
+    RUN_TEST(test_random_phase1_solving);
+
+    return UNITY_END();
+}
