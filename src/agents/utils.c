9d42e632a66780203b8fc61fd079d4effde3775b
h3nnn4n <uber.renan@gmail.com>
2021-07-10 13:51:40 -0300

    Add helper for agents

---
+++ src/agents/utils.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright <2021> <Renan S Silva, aka h3nnn4n>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include <grid.h>
+#include <snake.h>
+#include <utils.h>
+
+#include "utils.h"
+
+direction_t get_safe_random_direction(grid_t *grid, direction_t last_direction) {
+    direction_t new_direction = RIGHT;
+
+    uint8_t max_tries = 50;
+
+    do {
+        max_tries--;
+        new_direction = get_random_direction();
+
+        if (new_direction == RIGHT && last_direction == LEFT)
+            continue;
+        if (new_direction == LEFT && last_direction == RIGHT)
+            continue;
+
+        if (new_direction == UP && last_direction == DOWN)
+            continue;
+        if (new_direction == DOWN && last_direction == UP)
+            continue;
+
+        if (is_snake_colliding(grid, new_direction))
+            continue;
+
+        break;
+    } while (max_tries > 0);
+
+    return new_direction;
+}
