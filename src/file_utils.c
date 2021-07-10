8d2a34e966a307b19e5071322020bd8dd4b6464d
h3nnn4n <uber.renan@gmail.com>
2021-07-10 12:07:30 -0300

    Add helpers for handling files

---
+++ src/file_utils.c
@@ -0,0 +1,81 @@
+/*
+ * Copyright <2021> <Renan S Silva, aka h3nnn4n>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <assert.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include "file_utils.h"
+
+void ensure_directory_exists(char *directory) {
+    if (file_exists(directory)) {
+        return;
+    }
+
+    mkdir_p(directory);
+}
+
+int file_exists(char *filepath) {
+    struct stat buffer = {0};
+
+    return (stat(filepath, &buffer) == 0);
+}
+
+// https://gist.github.com/JonathonReinhart/8c0d90191c38af2dcadb102c4e202950
+int mkdir_p(const char *path) {
+    assert(path != NULL);
+
+    const size_t len = strlen(path);
+    char         _path[PATH_MAX];
+    char *       p;
+
+    errno = 0;
+
+    if (len > sizeof(_path) - 1) {
+        errno = ENAMETOOLONG;
+        return -1;
+    }
+
+    strcpy(_path, path);
+
+    for (p = _path + 1; *p; p++) {
+        if (*p == '/') {
+            *p = '\0';
+
+            if (mkdir(_path, S_IRWXU) != 0) {
+                if (errno != EEXIST)
+                    return -1;
+            }
+
+            *p = '/';
+        }
+    }
+
+    if (mkdir(_path, S_IRWXU) != 0) {
+        if (errno != EEXIST)
+            return -1;
+    }
+
+    return 0;
+}
