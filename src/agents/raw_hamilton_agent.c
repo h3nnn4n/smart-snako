2ab8b0667fba46ecc6f58578e13a7814809da965
h3nnn4n <uber.renan@gmail.com>
2021-07-10 22:08:27 -0300

    Make raw_hamilton agent work on all cell sizes

--- src/agents/raw_hamilton_agent.c
+++ src/agents/raw_hamilton_agent.c
@@ -30,11 +30,26 @@
 #include "agent_utils.h"
 #include "raw_hamilton_agent.h"
 
+uint32_t cells_not_visited_count(grid_t *grid) {
+    uint32_t count = 0;
+
+    raw_hamilton_context_t *agent_context = (raw_hamilton_context_t *)grid->agent_context;
+
+    for (int y = 0; y < grid->height; y++) {
+        for (int x = 0; x < grid->width; x++) {
+            if (!agent_context->path[x][y].visited)
+                count++;
+        }
+    }
+
+    return count;
+}
+
 bool all_cells_visited(grid_t *grid) {
     raw_hamilton_context_t *agent_context = (raw_hamilton_context_t *)grid->agent_context;
 
-    for (int x = 0; x < grid->width; x++) {
-        for (int y = 0; y < grid->width; y++) {
+    for (int y = 0; y < grid->height; y++) {
+        for (int x = 0; x < grid->width; x++) {
             if (!agent_context->path[x][y].visited)
                 return false;
         }
@@ -43,13 +58,51 @@ bool all_cells_visited(grid_t *grid) {
     return true;
 }
 
-bool build_halmiton_with_dfs(grid_t *grid, uint8_t x, uint8_t y) {
+void print_path(grid_t *grid) {
     raw_hamilton_context_t *agent_context = (raw_hamilton_context_t *)grid->agent_context;
-    static direction_t      directions[4] = {RIGHT, DOWN, UP, LEFT};
+    for (int y = 0; y < grid->height; y++) {
+        for (int x = 0; x < grid->width; x++) {
+            if (agent_context->path[x][y].visited) {
+                switch (agent_context->path[x][y].next_direction) {
+                    case RIGHT: printf("> "); break;
+                    case LEFT: printf("< "); break;
+                    case UP: printf("^ "); break;
+                    case DOWN: printf("V "); break;
+                }
+            } else {
+                printf(". ");
+            }
+        }
+        printf("\n");
+    }
+}
 
-    if (x == 0 && y == 0 && agent_context->path[x][y].visited)
-        if (all_cells_visited(grid))
+bool build_halmiton_with_dfs(grid_t *grid, uint8_t x, uint8_t y) {
+    raw_hamilton_context_t *agent_context   = (raw_hamilton_context_t *)grid->agent_context;
+    static direction_t      directions[4]   = {RIGHT, DOWN, UP, LEFT};
+    static direction_t      directions_2[4] = {LEFT, RIGHT, UP, DOWN};
+
+    // The two left-most columns, whe the width is odd and we are not on the first row
+    bool enable_odd_width_hack = (grid->width % 2 == 1) && (x < 2) && (y > 0);
+
+    // When both the rows and columns are an odd number
+    bool enable_odd_height_hack = (grid->height % 2 == 1) && (grid->height % 2 == 1);
+
+    if (x == 0 && y == 0 && agent_context->path[x][y].visited) {
+        /*printf("\n");*/
+        /*print_path(grid);*/
+        /*printf("\n");*/
+
+        if (all_cells_visited(grid)) {
             return true;
+        }
+        /*printf("aaaa %u %u\n", cells_not_visited_count(grid), enable_odd_height_hack);*/
+
+        if (cells_not_visited_count(grid) == 1 && enable_odd_height_hack) {
+            /*printf("hack\n");*/
+            return true;
+        }
+    }
 
     if (agent_context->path[x][y].visited)
         return false;
@@ -57,7 +110,11 @@ bool build_halmiton_with_dfs(grid_t *grid, uint8_t x, uint8_t y) {
     agent_context->path[x][y].visited = true;
 
     for (int i = 0; i < 4; i++) {
-        direction_t direction                    = directions[i];
+        direction_t direction = directions[i];
+
+        if (enable_odd_width_hack)
+            direction = directions_2[i];
+
         agent_context->path[x][y].next_direction = direction;
 
         uint8_t new_x = x;
--- test/test_raw_hamilto_agent.c
+++ test/test_raw_hamilto_agent.c
@@ -6,17 +6,21 @@
 #include <agents/raw_hamilton_agent.h>
 
 void test_build_halmiton_with_dfs() {
-    char message[512];
+    for (int width = 5; width <= 10; width++) {
+        for (int height = 5; height <= 10; height++) {
+            uint32_t not_visited_expected_count = 0;
 
-    for (int width = 6; width < 10; width++) {
-        for (int height = 6; height < 10; height++) {
-            snprintf(message, sizeof(message), "testing %d %d", width, height);
-            TEST_MESSAGE(message);
+            if (width % 2 == 1 && height % 2 == 1)
+                not_visited_expected_count = 1;
+
+            /*char message[512];*/
+            /*snprintf(message, sizeof(message), "testing %d %d", width, height);*/
+            /*TEST_MESSAGE(message);*/
 
             grid_t *grid = create_grid(width, height);
             raw_hamilton_agent_create(grid);
 
-            TEST_ASSERT_TRUE(all_cells_visited(grid));
+            TEST_ASSERT_EQUAL(not_visited_expected_count, cells_not_visited_count(grid));
 
             raw_hamilton_agent_destroy(grid);
             destroy_grid(grid);
