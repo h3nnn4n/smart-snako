8cc53706b712170d6bd815aa5dbca50ea5fa1806
h3nnn4n <uber.renan@gmail.com>
2021-07-24 23:22:37 -0300

    Improve test coverage (+ make test fail (it gets stuck))

--- test/test_hamilton_utils.c
+++ test/test_hamilton_utils.c
@@ -109,6 +109,45 @@ void test__apply_splice_random() {
     destroy_grid(grid);
 }
 
+// Failing because it is getting stuck in a loop
+void test__apply_splice_random_super() {
+    uint8_t width  = 20;
+    uint8_t height = 20;
+
+    coord_t source = {.x = 0, .y = 0};
+    coord_t target = {.x = (uint8_t)(width / 2), .y = (uint8_t)(height / 2)};
+
+    grid_t *         grid          = create_grid(width, height);
+    graph_context_t *graph_context = create_graph_context(grid);
+
+    build_halmiton_with_dfs(graph_context);
+    TEST_ASSERT_TRUE(is_graph_fully_connected(graph_context));
+
+    for (uint16_t i = 0; i < 1000; i++) {
+        printf("%d\n", i);
+        uint8_t x = get_random_number(width - 1);
+        uint8_t y = get_random_number(height - 1);
+
+        uint16_t original_distance   = path_distance(graph_context, source, target);
+        uint16_t original_path_count = tag_paths(graph_context);
+
+        // Not all random splices are possible
+        if (!_apply_splice(graph_context, (coord_t){.x = x, .y = y}))
+            continue;
+
+        TEST_ASSERT_TRUE(_apply_splice(graph_context, (coord_t){.x = x, .y = y}));
+
+        TEST_ASSERT_EQUAL(original_path_count, tag_paths(graph_context));
+        TEST_ASSERT_EQUAL(original_distance, path_distance(graph_context, source, target));
+
+        // Doing this results in several splices being applied on top of each other
+        TEST_ASSERT_TRUE(_apply_splice(graph_context, (coord_t){.x = x, .y = y}));
+    }
+
+    destroy_graph_context(graph_context);
+    destroy_grid(grid);
+}
+
 void test_perturbate_hamiltonian_cycle() {
     uint8_t width  = 20;
     uint8_t height = 20;
@@ -149,9 +188,10 @@ int main() {
     UNITY_BEGIN();
 
     RUN_TEST(test_build_halmiton_with_dfs_visits_all_cells);
-    RUN_TEST(test_perturbate_hamiltonian_cycle);
     RUN_TEST(test__apply_splice);
     RUN_TEST(test__apply_splice_random);
+    RUN_TEST(test__apply_splice_random_super);
+    RUN_TEST(test_perturbate_hamiltonian_cycle);
 
     return UNITY_END();
 }
