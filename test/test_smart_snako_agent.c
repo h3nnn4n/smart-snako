7c7fc0823605e91fbd91f2250de177f10f88cb3c
h3nnn4n <uber.renan@gmail.com>
2021-07-25 14:28:54 -0300

    Run smakrt snako agent test on a fixed size grid

--- test/test_smart_snako_agent.c
+++ test/test_smart_snako_agent.c
@@ -6,27 +6,26 @@
 #include <agents/smart_snako_agent.h>
 
 void test_agent() {
-    char *agent_name = "smart-snako";
-    for (int width = 20; width <= 25; width++) {
-        for (int height = 20; height <= 25; height++) {
-            grid_t *grid = create_grid(width, height);
-            set_agent_name(grid->stats, agent_name);
-            spawn_cherry(grid);
-            smart_snako_agent_create(grid);
+    char *  agent_name = "smart-snako";
+    uint8_t width      = 20;
+    uint8_t height     = 20;
 
-            while (!is_game_over(grid)) {
-                print_grid(grid);
-                print_stats(grid->stats);
-                direction_t direction = smart_snako_agent(grid);
-                simulate(grid, direction);
-            }
+    grid_t *grid = create_grid(width, height);
+    set_agent_name(grid->stats, agent_name);
+    spawn_cherry(grid);
+    smart_snako_agent_create(grid);
 
-            TEST_ASSERT_TRUE(grid->stats->cherries_eaten > 10);
-
-            smart_snako_agent_destroy(grid);
-            destroy_grid(grid);
-        }
+    while (!is_game_over(grid)) {
+        print_grid(grid);
+        print_stats(grid->stats);
+        direction_t direction = smart_snako_agent(grid);
+        simulate(grid, direction);
     }
+
+    TEST_ASSERT_TRUE(grid->stats->cherries_eaten > 10);
+
+    smart_snako_agent_destroy(grid);
+    destroy_grid(grid);
 }
 
 void setUp() {}
