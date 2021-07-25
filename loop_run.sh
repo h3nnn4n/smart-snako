3cc83e456eb443decef1bb3ab6ffef332b0610fe
h3nnn4n <uber.renan@gmail.com>
2021-07-25 19:26:11 -0300

    Update / Make it easier to runs multiple tests in parallel

---
+++ loop_run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+while [[ true ]]
+do
+    sh run.sh
+done
--- run.sh
+++ run.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-rm -rf stats
-
-make
-
 width=50
 height=50
 
-n_runs=100
+n_runs=10
 
-for agent in random manhattan dfs ida_dfs raw_hamilton
+for agent in random manhattan dfs ida_dfs raw_hamilton smart_snako
 do
     echo $agent
     for i in `seq $n_runs`
